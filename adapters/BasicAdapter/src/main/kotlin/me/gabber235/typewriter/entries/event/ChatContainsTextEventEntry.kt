f1c7169c5bf4a72c44918c3063d0f4bc4a0c735f
Caleb <calebneuf@gmail.com>
2023-02-06 23:51:46 -0700

    BasicAdapter: Added chat event for containing specific text

---
+++ adapters/BasicAdapter/src/main/kotlin/me/gabber235/typewriter/entries/event/ChatContainsTextEventEntry.kt
@@ -0,0 +1,25 @@
+package me.gabber235.typewriter.entries.event
+
+import io.papermc.paper.event.player.AsyncChatEvent
+import me.gabber235.typewriter.adapters.Colors
+import me.gabber235.typewriter.adapters.Entry
+import me.gabber235.typewriter.entry.*
+import me.gabber235.typewriter.entry.entries.EventEntry
+import me.gabber235.typewriter.utils.Icons
+import me.gabber235.typewriter.utils.plainText
+import java.util.*
+
+@Entry("on_message_contains_text", "When the player sends a chat message containing certain text", Colors.YELLOW, Icons.NOTE_STICKY)
+class ChatContainsTextEventEntry(
+	override val id: String = "",
+	override val name: String = "",
+	override val triggers: List<String> = emptyList(),
+	val text: String = "",
+) : EventEntry
+
+
+@EntryListener(ChatContainsTextEventEntry::class)
+fun onChat(event: AsyncChatEvent, query: Query<ChatContainsTextEventEntry>) {
+	val message = event.message()
+	query findWhere { Regex(it.text).matches(message.plainText()) } triggerAllFor event.player
+}
