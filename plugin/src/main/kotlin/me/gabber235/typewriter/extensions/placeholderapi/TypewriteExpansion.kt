4119f61ed5a791a7aed1622148f63c88daed9f11
Gabber235 <gabber200@icloud.com>
2023-02-09 19:53:20 +0100

    use query instead of the database directly

--- plugin/src/main/kotlin/me/gabber235/typewriter/extensions/placeholderapi/TypewriteExpansion.kt
+++ plugin/src/main/kotlin/me/gabber235/typewriter/extensions/placeholderapi/TypewriteExpansion.kt
@@ -4,7 +4,6 @@ import lirand.api.extensions.server.server
 import me.clip.placeholderapi.PlaceholderAPI
 import me.clip.placeholderapi.expansion.PlaceholderExpansion
 import me.gabber235.typewriter.Typewriter
-import me.gabber235.typewriter.entry.EntryDatabase
 import me.gabber235.typewriter.entry.Query
 import me.gabber235.typewriter.entry.entries.SpeakerEntry
 import me.gabber235.typewriter.facts.FactDatabase
@@ -23,11 +22,7 @@ object TypewriteExpansion : PlaceholderExpansion() {
 
 		if (params.startsWith("speaker_")) {
 			val speakerName = params.substring(8)
-			val speaker =
-				Query.findByName(speakerName) ?: EntryDatabase.getEntity<SpeakerEntry>(
-					speakerName
-				)
-				?: return null
+			val speaker: SpeakerEntry = Query.findByName(speakerName) ?: Query.findById(speakerName) ?: return null
 			return speaker.displayName
 		}
 
