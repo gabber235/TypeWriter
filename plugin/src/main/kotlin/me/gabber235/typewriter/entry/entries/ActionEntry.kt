b693dc80d8404dd250cc1cd33becbf1c7eeed919
Gabber235 <gabber200@icloud.com>
2023-02-11 11:34:44 +0100

    Create CustomTriggeringActionEntry
    For when you need to customise the triggering of triggers

--- plugin/src/main/kotlin/me/gabber235/typewriter/entry/entries/ActionEntry.kt
+++ plugin/src/main/kotlin/me/gabber235/typewriter/entry/entries/ActionEntry.kt
@@ -1,7 +1,10 @@
 package me.gabber235.typewriter.entry.entries
 
 import me.gabber235.typewriter.adapters.Tags
+import me.gabber235.typewriter.adapters.modifiers.EntryIdentifier
+import me.gabber235.typewriter.adapters.modifiers.Triggers
 import me.gabber235.typewriter.entry.TriggerableEntry
+import me.gabber235.typewriter.entry.triggerEntriesFor
 import me.gabber235.typewriter.facts.FactDatabase
 import org.bukkit.entity.Player
 
@@ -9,5 +12,20 @@ import org.bukkit.entity.Player
 interface ActionEntry : TriggerableEntry {
 	fun execute(player: Player) {
 		FactDatabase.modify(player.uniqueId, modifiers)
+	}
+}
+
+@Tags("custom_triggering_action")
+interface CustomTriggeringActionEntry : ActionEntry {
+	// Disable the normal triggers. So that the action can manually trigger the next actions.
+	override val triggers: List<String>
+		get() = emptyList()
+
+	@Triggers
+	@EntryIdentifier(TriggerableEntry::class)
+	val customTriggers: List<String>
+
+	fun Player.triggerCustomTriggers() {
+		customTriggers triggerEntriesFor this
 	}
 }
