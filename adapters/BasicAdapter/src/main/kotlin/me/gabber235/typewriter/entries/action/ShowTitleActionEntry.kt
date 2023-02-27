d46c182d8bd6b7767b76301bde57bcafa1f42d25
Caleb <calebneuf@gmail.com>
2023-02-27 10:10:48 -0700

    Action entry to show title for BasicAdapter
    Not sure if it translates colors or not.. haven't worked with Adventure before

---
+++ adapters/BasicAdapter/src/main/kotlin/me/gabber235/typewriter/entries/action/ShowTitleActionEntry.kt
@@ -0,0 +1,64 @@
+package me.gabber235.typewriter.entries.action
+
+import me.gabber235.typewriter.adapters.Colors
+import me.gabber235.typewriter.adapters.Entry
+import me.gabber235.typewriter.adapters.modifiers.Help
+import me.gabber235.typewriter.entry.Criteria
+import me.gabber235.typewriter.entry.Modifier
+import me.gabber235.typewriter.entry.entries.ActionEntry
+import me.gabber235.typewriter.utils.Icons
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.title.Title
+import org.bukkit.entity.Player
+import java.time.Duration
+import java.util.*
+
+@Entry("show_title", "Show a title to a player", Colors.RED, Icons.ALIGN_CENTER)
+data class ShowTitleActionEntry(
+	override val id: String = "",
+	override val name: String = "",
+	override val criteria: List<Criteria> = emptyList(),
+	override val modifiers: List<Modifier> = emptyList(),
+	override val triggers: List<String> = emptyList(),
+	@Help("The title text to show.")
+	val title: String = "",
+	@Help("The subtitle text to show.")
+	val subtitle: String = "",
+	@Help("Optional duration settings for the title.")
+	val durations: Optional<TitleDurations> = Optional.empty(),
+) : ActionEntry {
+	override fun execute(player: Player) {
+		super.execute(player)
+
+		val adventureTitle: Title
+
+		if(durations.isPresent) {
+			adventureTitle = Title.title(
+			Component.text(title),
+			Component.text(subtitle),
+			Title.Times.times(
+				Duration.ofMillis(durations.get().fadeIn),
+				Duration.ofMillis(durations.get().stay),
+				Duration.ofMillis(durations.get().fadeOut)
+			)
+			)
+		} else {
+			adventureTitle = Title.title(
+			Component.text(title),
+			Component.text(subtitle)
+			)
+		}
+
+		player.showTitle(adventureTitle)
+
+	}
+}
+
+data class TitleDurations(
+	@Help("The duration of the fade in effect. (In milliseconds)")
+	val fadeIn: Long,
+	@Help("The duration that it stays. (In milliseconds)")
+	val stay: Long,
+	@Help("The duration of the fade out effect. (In milliseconds)")
+	val fadeOut: Long
+)
