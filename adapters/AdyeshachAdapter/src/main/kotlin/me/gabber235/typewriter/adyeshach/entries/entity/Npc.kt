da3d91208d387e893366a5ddf704eaf421ea9e3f
ahdg <36436808+ahdg6@users.noreply.github.com>
2023-03-15 07:41:46 +0800

    remove GriefDefender and Adyeshach

--- adapters/AdyeshachAdapter/src/main/kotlin/me/gabber235/typewriter/adyeshach/entries/entity/Npc.kt
+++
@@ -1,7 +0,0 @@
-package me.gabber235.typewriter.adyeshach.entries.entity
-
-import me.gabber235.typewriter.adapters.Tags
-import me.gabber235.typewriter.entry.entries.SpeakerEntry
-
-@Tags("npc")
-interface Npc : SpeakerEntry
--- adapters/GriefDefenderAdapter/settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-rootProject.name = "GriefDefenderAdapter"
-
-includeBuild("../../plugin")
