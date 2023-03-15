b1aeb3009253f246df93a71031c8edc707e6cac4
ahdg <36436808+ahdg6@users.noreply.github.com>
2023-03-15 08:52:40 +0800

    documentation

--- documentation/docs/adapters/MythicMobsAdapter/generate_docs.bat
+++ documentation/docs/adapters/MythicMobsAdapter/generate_docs.bat
@@ -1,3 +1,3 @@
 cd ../
-python parse.py "Vault Adapter" "The Vault Adapter is an adapter for the Vault plugin. It allows you to use Vault's economy system in your dialogue." ./VaultAdapter/ ../../../adapters/VaultAdapter/src/main/kotlin/com/caleb/typewriter/vault/entries
+python parse.py "MythicMobs Adapter" "The MythicMobs Adapter is an adapter for the MythicMobs plugin. " ./MythicMobsAdapter/ ../../../adapters/MythicMobsAdapter/src/main/kotlin/com/caleb/typewriter/mythicmobs/entries
 pause
--- documentation/docs/adapters/RPGRegionsAdapter/generate_docs.bat
+++ documentation/docs/adapters/RPGRegionsAdapter/generate_docs.bat
@@ -1,3 +1,3 @@
 cd ../
-python parse.py "Vault Adapter" "The Vault Adapter is an adapter for the Vault plugin. It allows you to use Vault's economy system in your dialogue." ./VaultAdapter/ ../../../adapters/VaultAdapter/src/main/kotlin/com/caleb/typewriter/vault/entries
+python parse.py "RPGRegions Adapter" "The RPGRegions Adapter is an adapter for the RPGRegions plugin." ./RPGRegionsAdapter/ ../../../adapters/RPGRegionsAdapter/src/main/kotlin/com/caleb/typewriter/rpgregions/entries
 pause
