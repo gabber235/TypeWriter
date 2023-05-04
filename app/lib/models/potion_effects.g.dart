fb79accef4b51d1a2f8527ac3b6512ac8a2a5579
Gabber235 <gabber200@icloud.com>
2023-05-04 20:55:26 +0200

    Create custom editor for `PotionEffectType`

---
+++ app/lib/models/potion_effects.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'potion_effects.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$potionEffectsHash() => r'298f4c5da058f2f6701ba11c05906db53dbfbde3';
+
+/// See also [potionEffects].
+@ProviderFor(potionEffects)
+final potionEffectsProvider = AutoDisposeFutureProvider<List<String>>.internal(
+  potionEffects,
+  name: r'potionEffectsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$potionEffectsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef PotionEffectsRef = AutoDisposeFutureProviderRef<List<String>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
---
+++ app/lib/widgets/inspector/editors/potion_effect.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'potion_effect.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fuzzyPotionEffectsHash() =>
+    r'd5c5bf40d115e6596753136f3f078967662ca52f';
+
+/// See also [_fuzzyPotionEffects].
+@ProviderFor(_fuzzyPotionEffects)
+final _fuzzyPotionEffectsProvider = AutoDisposeProvider<Fuzzy<String>>.internal(
+  _fuzzyPotionEffects,
+  name: r'_fuzzyPotionEffectsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$fuzzyPotionEffectsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _FuzzyPotionEffectsRef = AutoDisposeProviderRef<Fuzzy<String>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
