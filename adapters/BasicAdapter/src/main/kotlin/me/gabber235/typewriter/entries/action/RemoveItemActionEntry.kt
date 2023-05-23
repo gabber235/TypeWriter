e9835634e154addc0746db3cab892f2fd5b27df5
PxlPioneer <r.e.latcham92@gmail.com>
2023-05-23 15:12:03 +0100

    Added RemoveItem entry

---
+++ adapters/BasicAdapter/src/main/kotlin/me/gabber235/typewriter/entries/action/RemoveItemActionEntry.kt
@@ -0,0 +1,32 @@
+package me.gabber235.typewriter.entries.action
+
+import me.gabber235.typewriter.adapters.Colors
+import me.gabber235.typewriter.adapters.Entry
+import me.gabber235.typewriter.adapters.modifiers.Help
+import me.gabber235.typewriter.entry.Criteria
+import me.gabber235.typewriter.entry.Modifier
+import me.gabber235.typewriter.entry.entries.ActionEntry
+import me.gabber235.typewriter.utils.Icons
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+
+@Entry("remove_item", "Remove an from players inventory", Colors.RED, Icons.WAND_SPARKLES)
+class RemoveItemActionEntry(
+    override val id: String = "",
+    override val name: String = "",
+    override val criteria: List<Criteria>,
+    override val modifiers: List<Modifier>,
+    override val triggers: List<String> = emptyList(),
+    @Help("The item to remove.")
+    private val material: Material = Material.AIR,
+    @Help("The amount of items to remove.")
+    private val amount: Int = 1,
+) : ActionEntry {
+    override fun execute(player: Player) {
+        super.execute(player)
+
+        val item = ItemStack(material, amount)
+        player.inventory.removeItemAnySlot(item)
+    }
+}
