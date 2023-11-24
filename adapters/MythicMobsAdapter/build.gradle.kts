93d8f04921ba1d8f85a8978a62f787d50686dcfd
Gabber235 <gabber200@icloud.com>
2023-11-24 17:56:02 +0100

    Fix jar outputs

--- adapters/MythicMobsAdapter/build.gradle.kts
+++ adapters/MythicMobsAdapter/build.gradle.kts
@@ -90,5 +90,6 @@ task<ShadowJar>("buildRelease") {
         // Rename the jar to remove the version and -all
         val jar = file("build/libs/%s-%s-all.jar".format(project.name, project.version))
         jar.renameTo(file("build/libs/%s.jar".format(project.name)))
+        file("build/libs/%s-%s.jar".format(project.name, project.version)).delete()
     }
 }
--- adapters/SuperiorSkyblockAdapter/build.gradle.kts
+++ adapters/SuperiorSkyblockAdapter/build.gradle.kts
@@ -90,5 +90,6 @@ task<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("buildRelease")
         // Rename the jar to remove the version and -all
         val jar = file("build/libs/%s-%s-all.jar".format(project.name, project.version))
         jar.renameTo(file("build/libs/%s.jar".format(project.name)))
+        file("build/libs/%s-%s.jar".format(project.name, project.version)).delete()
     }
 }
