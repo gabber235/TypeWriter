bf1442ba459550038011c5bc8181517d98184e74
Gabber235 <gabber200@icloud.com>
2023-11-28 16:41:23 +0100

    Fix flutter text size calculations

--- app/lib/hooks/text_size.dart
+++ app/lib/hooks/text_size.dart
@@ -1,10 +1,6 @@
 import "package:flutter/material.dart";
 import "package:flutter_hooks/flutter_hooks.dart";
 
-/// For some reason the differences between the Text widget and the TextPainter
-/// are not the same. This is a correction factor to make them the same.
-const flutterPainterErrorCorrection = 1.035;
-
 /// Returns the size of the text with the given [style] in the given [context].
 Size useTextSize(BuildContext context, String text, [TextStyle? style]) {
   return useMemoized(
@@ -15,21 +11,19 @@ Size useTextSize(BuildContext context, String text, [TextStyle? style]) {
       final textPainter = TextPainter(
         text: TextSpan(
           text: text,
-          style:
-              style?.apply(fontFamily: style.fontFamily ?? defaultFontFamily) ??
-                  DefaultTextStyle.of(context).style,
+          style: style?.copyWith(
+                fontFamily: style.fontFamily ?? defaultFontFamily,
+                letterSpacing:
+                    style.letterSpacing ?? defaultTextStyle.letterSpacing,
+              ) ??
+              DefaultTextStyle.of(context).style,
         ),
         maxLines: 1,
         textScaler: MediaQuery.of(context).textScaler,
         textDirection: TextDirection.ltr,
       )..layout(minWidth: 0, maxWidth: double.infinity);
 
-      // TODO: Remove this once the bug is fixed
-      final size = textPainter.size;
-      return Size(
-        (size.width * flutterPainterErrorCorrection).roundToDouble(),
-        size.height,
-      );
+      return textPainter.size;
     },
     [text, style],
   );
