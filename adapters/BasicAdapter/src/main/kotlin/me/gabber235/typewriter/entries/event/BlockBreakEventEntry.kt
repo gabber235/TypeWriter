2bae5f4c65947a8c1ae2ac2bc00d7d8483ee3874
Gabber235 <gabber200@icloud.com>
2024-01-04 14:12:05 +0100

    Move to the new Item type

--- adapters/BasicAdapter/src/main/kotlin/me/gabber235/typewriter/entries/event/BlockBreakEventEntry.kt
+++ adapters/BasicAdapter/src/main/kotlin/me/gabber235/typewriter/entries/event/BlockBreakEventEntry.kt
@@ -1,15 +1,17 @@
 package me.gabber235.typewriter.entries.event
 
+import com.google.gson.JsonObject
+import lirand.api.extensions.other.set
 import me.gabber235.typewriter.adapters.Colors
 import me.gabber235.typewriter.adapters.Entry
 import me.gabber235.typewriter.adapters.modifiers.Help
 import me.gabber235.typewriter.adapters.modifiers.MaterialProperties
 import me.gabber235.typewriter.adapters.modifiers.MaterialProperty
-import me.gabber235.typewriter.entry.EntryListener
-import me.gabber235.typewriter.entry.Query
+import me.gabber235.typewriter.entry.*
 import me.gabber235.typewriter.entry.entries.EventEntry
-import me.gabber235.typewriter.entry.startDialogueWithOrNextDialogue
 import me.gabber235.typewriter.utils.Icons
+import me.gabber235.typewriter.utils.Item
+import me.gabber235.typewriter.utils.optional
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.entity.Player
@@ -28,18 +30,20 @@ class BlockBreakEventEntry(
     override val id: String = "",
     override val name: String = "",
     override val triggers: List<String> = emptyList(),
-    @Help("The location of the block that was broken.")
-    val location: Optional<Location> = Optional.empty(),
-    @MaterialProperties(MaterialProperty.ITEM)
-    @Help("The item the player must be holding when the block is broken.")
-    val itemInHand: Optional<Material> = Optional.empty(),
     @MaterialProperties(MaterialProperty.BLOCK)
     @Help("The block that was broken.")
     val block: Optional<Material> = Optional.empty(),
+    @Help("The location of the block that was broken.")
+    val location: Optional<Location> = Optional.empty(),
+    @Help("The item the player must be holding when the block is broken.")
+    val itemInHand: Item = Item.Empty,
 ) : EventEntry
 
-private fun hasMaterialInHand(player: Player, material: Material): Boolean {
-    return player.inventory.itemInMainHand.type == material || player.inventory.itemInOffHand.type == material
+private fun hasItemInHand(player: Player, item: Item): Boolean {
+    return item.isSameAs(player, player.inventory.itemInMainHand) || item.isSameAs(
+        player,
+        player.inventory.itemInOffHand
+    )
 }
 
 @EntryListener(BlockBreakEventEntry::class)
@@ -49,9 +53,24 @@ fun onBlockBreak(event: BlockBreakEvent, query: Query<BlockBreakEventEntry>) {
         if (!entry.location.map { it == event.block.location }.orElse(true)) return@findWhere false
 
         // Check if the player is holding the correct item
-        if (!entry.itemInHand.map { hasMaterialInHand(event.player, it) }.orElse(true)) return@findWhere false
+        if (!hasItemInHand(event.player, entry.itemInHand)) return@findWhere false
 
         // Check if block type is correct
         entry.block.map { it == event.block.type }.orElse(true)
     } startDialogueWithOrNextDialogue event.player
+}
+
+@EntryMigration(BlockBreakEventEntry::class, "0.4.0")
+@NeedsMigrationIfNotParsable
+fun migrate040BlockBreakEvent(json: JsonObject, context: EntryMigratorContext): JsonObject {
+    val data = JsonObject()
+    data.copyAllBut(json, "itemInHand")
+
+    val itemInHand = json.getAndParse<Optional<Material>>("itemInHand", context.gson).optional
+
+    val item = Item(material = itemInHand)
+
+    data["itemInHand"] = context.gson.toJsonTree(item)
+
+    return data
 }
