da25bb152889dd7f037394a29d1aa254175a7194
Gabber235 <gabber200@icloud.com>
2024-02-09 22:32:11 +0100

    Exclude Netty

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -106,6 +106,7 @@ tasks.withType<ShadowJar> {
         exclude(dependency("org.jetbrains.kotlin:kotlin-stdlib"))
         exclude(dependency("org.jetbrains.kotlin:kotlin-reflect"))
         exclude(dependency("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core"))
+        exclude("io.netty")
     }
 }
 
