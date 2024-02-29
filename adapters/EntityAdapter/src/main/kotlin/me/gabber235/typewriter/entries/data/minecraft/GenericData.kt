9e4eeed34fce7416f1c1cbb4f4db2a6583452b82
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-02-29 20:23:52 +0100

    Update GenericData.kt

--- adapters/EntityAdapter/src/main/kotlin/me/gabber235/typewriter/entries/data/minecraft/GenericData.kt
+++ adapters/EntityAdapter/src/main/kotlin/me/gabber235/typewriter/entries/data/minecraft/GenericData.kt
@@ -7,6 +7,9 @@ import me.tofaa.entitylib.wrapper.WrapperEntity
 fun applyGenericEntityData(entity: WrapperEntity, property: EntityProperty): Boolean {
     when (property) {
         is OnFireProperty -> applyOnFireData(entity, property)
+        is GlowingEffectProperty -> applyGlowingEffectData(entity, property)
+        is PoseProperty -> applyPoseData(entity, property)
+        is CustomNameProperty -> applyCustomNameData(entity, property)
         else -> return false
     }
 
