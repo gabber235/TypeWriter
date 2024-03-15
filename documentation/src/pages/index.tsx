b8b0befd09253c1d2ab761d772d42377c5720a85
Gabber235 <gabber200@icloud.com>
2024-03-15 17:40:13 +0100

    Space version badge

--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Rive from "@rive-app/react-canvas";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Layout from "@theme/Layout";
 import clsx from "clsx";
@@ -9,52 +8,52 @@ import React from "react";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+    const { siteConfig } = useDocusaurusContext();
 
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      {/* Video Background */}
-      <div id="video-container" className={styles.videoContainer}>
-      <video autoPlay loop muted className={styles.videoBackground}>
-        <source
-          src={require("@site/static/videos/backgroundvideo-homepage.webm").default}
-          type="video/webm"
-        />
-        Your browser does not support the video tag.
-      </video>
-        
-        {/* Add overlay element */}
-        <div className={styles.overlay}></div>
-      </div>
+    return (
+        <header className={clsx("hero hero--primary", styles.heroBanner)}>
+            {/* Video Background */}
+            <div id="video-container" className={styles.videoContainer}>
+                <video autoPlay loop muted className={styles.videoBackground}>
+                    <source
+                        src={require("@site/static/videos/backgroundvideo-homepage.webm").default}
+                        type="video/webm"
+                    />
+                    Your browser does not support the video tag.
+                </video>
 
-      {/* Your existing content */}
-      <div className={clsx("container", styles.contentContainer)}>
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className={clsx(
-              "button button--secondary button--lg",
-              styles.startButton
-            )}
-            to="/docs/home"
-          >
-            Get Started →
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+                {/* Add overlay element */}
+                <div className={styles.overlay}></div>
+            </div>
+
+            {/* Your existing content */}
+            <div className={clsx("container", styles.contentContainer)}>
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className={clsx(
+                            "button button--secondary button--lg",
+                            styles.startButton
+                        )}
+                        to="/docs/home"
+                    >
+                        Get Started →
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout title={`${siteConfig.title}`} description={`${siteConfig.tagline}`}>
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <Layout title={`${siteConfig.title}`} description={`${siteConfig.tagline}`}>
+            <HomepageHeader />
+            <main>
+                <HomepageFeatures />
+            </main>
+        </Layout>
+    );
 }
