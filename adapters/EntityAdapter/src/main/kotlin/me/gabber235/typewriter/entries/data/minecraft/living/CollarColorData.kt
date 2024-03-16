98aefba8020e84ad1d5d0d45b1f391ae8a8951d7
Gabber235 <gabber200@icloud.com>
2024-03-16 18:11:55 +0100

    Unify Collar

--- adapters/EntityAdapter/src/main/kotlin/me/gabber235/typewriter/entries/data/minecraft/living/CollarColorData.kt
+++ adapters/EntityAdapter/src/main/kotlin/me/gabber235/typewriter/entries/data/minecraft/living/CollarColorData.kt
@@ -1,4 +1,4 @@
-package me.gabber235.typewriter.entries.data.minecraft.living.cat
+package me.gabber235.typewriter.entries.data.minecraft.living
 
 import me.gabber235.typewriter.adapters.Colors
 import me.gabber235.typewriter.adapters.Entry
@@ -7,18 +7,18 @@ import me.gabber235.typewriter.adapters.modifiers.Help
 import me.gabber235.typewriter.entry.entity.SinglePropertyCollectorSupplier
 import me.gabber235.typewriter.entry.entries.EntityData
 import me.gabber235.typewriter.entry.entries.EntityProperty
-import me.gabber235.typewriter.entry.entries.GenericEntityData
 import me.gabber235.typewriter.extensions.packetevents.metas
 import me.tofaa.entitylib.extras.DyeColor
 import me.tofaa.entitylib.meta.mobs.tameable.CatMeta
+import me.tofaa.entitylib.meta.mobs.tameable.WolfMeta
 import me.tofaa.entitylib.wrapper.WrapperEntity
 import org.bukkit.entity.Player
 import java.util.*
 import kotlin.reflect.KClass
 
-@Entry("cat_collar_color_data", "The color of the cat's collar", Colors.RED, "mdi:cat")
-@Tags("cat_data", "collar_color_data")
-class CollarColorData (
+@Entry("collar_color_data", "The color of the cat's or wolfs collar", Colors.RED, "fluent:paint-bucket-16-filled")
+@Tags("cat_data", "wolf_data", "collar_color_data")
+class CollarColorData(
     override val id: String = "",
     override val name: String = "",
     @Help("The color of the cat's collar.")
@@ -30,13 +30,14 @@ class CollarColorData (
     override fun build(player: Player): CollarColorProperty = CollarColorProperty(catCollarColor)
 }
 
-data class CollarColorProperty(val catCollarColor: DyeColor) : EntityProperty {
+data class CollarColorProperty(val collarColor: DyeColor) : EntityProperty {
     companion object : SinglePropertyCollectorSupplier<CollarColorProperty>(CollarColorProperty::class)
 }
 
 fun applyCollarColorData(entity: WrapperEntity, property: CollarColorProperty) {
     entity.metas {
-        meta<CatMeta> { collarColor = property.catCollarColor }
+        meta<CatMeta> { collarColor = property.collarColor }
+        meta<WolfMeta> { collarColor = property.collarColor.ordinal }
         error("Could not apply CatCollarColorData to ${entity.entityType} entity.")
     }
 }
