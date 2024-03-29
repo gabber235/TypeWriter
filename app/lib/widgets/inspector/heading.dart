292f1c267a45c31491358e811605109024faf66e
Gabber235 <gabber200@icloud.com>
2024-03-29 20:31:35 +0100

    Remove unused import

--- app/lib/widgets/inspector/heading.dart
+++ app/lib/widgets/inspector/heading.dart
@@ -1,6 +1,5 @@
 import "package:auto_size_text/auto_size_text.dart";
 import "package:flutter/material.dart";
-import "package:flutter/widgets.dart";
 import "package:flutter_hooks/flutter_hooks.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:riverpod_annotation/riverpod_annotation.dart";
