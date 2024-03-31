f5828f10169be2a8cc30579ef358e1302e918d32
Gabber235 <gabber200@icloud.com>
2024-03-31 10:29:45 +0200

    Typo

--- documentation/src/components/UnsupportedWarning/index.tsx
+++ documentation/src/components/UnsupportedWarning/index.tsx
@@ -12,7 +12,7 @@ export default function UnsupportedWarning(
         <Admonition type="danger" title="Unsupported Warning">
             <p>
                 This {props.adapter ? 'Adapter' : 'Entry'} has been marked as unsupported.
-                This means that you will not get any support for this {props.adapter ? 'Adapter' : 'Entry'} exept from bugfixes.<br />
+                This means that you will not get any support for this {props.adapter ? 'Adapter' : 'Entry'} except from bugfixes.<br />
                 <b>It may be removed from the repository at any time.</b>
             </p>
         </Admonition>
