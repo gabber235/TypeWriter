ae73af01f22d5ca9ea4fac28653eebbf03d4d2eb
Kerzinator_24 <kerzinator24@gmail.com>
2024-05-11 11:01:35 +0200

    added skeleton entity

---
+++ adapters/EntityAdapter/src/main/kotlin/me/gabber235/typewriter/entries/entity/minecraft/SkeletonEntity.kt
@@ -0,0 +1,63 @@
+package me.gabber235.typewriter.entries.entity.minecraft
+
+import com.github.retrooper.packetevents.protocol.entity.type.EntityTypes
+import me.gabber235.typewriter.adapters.Colors
+import me.gabber235.typewriter.adapters.Entry
+import me.gabber235.typewriter.adapters.Tags
+import me.gabber235.typewriter.adapters.modifiers.OnlyTags
+import me.gabber235.typewriter.entries.data.minecraft.applyGenericEntityData
+import me.gabber235.typewriter.entries.data.minecraft.living.AgableProperty
+import me.gabber235.typewriter.entries.data.minecraft.living.applyAgeableData
+import me.gabber235.typewriter.entries.data.minecraft.living.applyLivingEntityData
+import me.gabber235.typewriter.entry.Ref
+import me.gabber235.typewriter.entry.emptyRef
+import me.gabber235.typewriter.entry.entity.FakeEntity
+import me.gabber235.typewriter.entry.entity.SimpleEntityDefinition
+import me.gabber235.typewriter.entry.entity.SimpleEntityInstance
+import me.gabber235.typewriter.entry.entity.WrapperFakeEntity
+import me.gabber235.typewriter.entry.entries.EntityActivityEntry
+import me.gabber235.typewriter.entry.entries.EntityData
+import me.gabber235.typewriter.entry.entries.EntityProperty
+import me.gabber235.typewriter.utils.Sound
+import org.bukkit.Location
+import org.bukkit.entity.Player
+
+@Entry("skeleton_definition", "A skeleton entity", Colors.ORANGE, "icon-park-outline:pig")
+@Tags("skeleton_definition")
+/**
+ * The `SkeletonDefinition` class is an entry that shows up as a skeleton in-game.
+ *
+ * ## How could this be used?
+ * This could be used to create a skeleton entity.
+ */
+class SkeletonDefinition(
+    override val id: String = "",
+    override val name: String = "",
+    override val displayName: String = "",
+    override val sound: Sound = Sound.EMPTY,
+    @OnlyTags("generic_entity_data", "living_entity_data", "mob_data")
+    override val data: List<Ref<EntityData<*>>> = emptyList(),
+) : SimpleEntityDefinition {
+    override fun create(player: Player): FakeEntity = SkeletonEntity(player)
+}
+
+@Entry("skeleton_instance", "An instance of a skeleton entity", Colors.YELLOW, "icon-park-outline:pig")
+class SkeletonInstance(
+    override val id: String = "",
+    override val name: String = "",
+    override val definition: Ref<SkeletonDefinition> = emptyRef(),
+    override val spawnLocation: Location = Location(null, 0.0, 0.0, 0.0),
+    @OnlyTags("generic_entity_data", "living_entity_data", "mob_data")
+    override val data: List<Ref<EntityData<*>>> = emptyList(),
+    override val activities: List<Ref<out EntityActivityEntry>> = emptyList(),
+) : SimpleEntityInstance
+
+private class SkeletonEntity(player: Player) : WrapperFakeEntity(
+    EntityTypes.HOGLIN,
+    player,
+) {
+    override fun applyProperty(property: EntityProperty) {
+        if (applyGenericEntityData(entity, property)) return
+        if (applyLivingEntityData(entity, property)) return
+    }
+}
