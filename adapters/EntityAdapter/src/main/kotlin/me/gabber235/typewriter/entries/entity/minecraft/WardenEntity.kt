ae30ff57bbf1694c13288377d2d723d52af7ea2c
Kerzinator_24 <kerzinator24@gmail.com>
2024-05-11 15:30:16 +0200

    added warden entity

---
+++ adapters/EntityAdapter/src/main/kotlin/me/gabber235/typewriter/entries/entity/minecraft/WardenEntity.kt
@@ -0,0 +1,67 @@
+package me.gabber235.typewriter.entries.entity.minecraft
+
+import com.github.retrooper.packetevents.protocol.entity.type.EntityTypes
+import me.gabber235.typewriter.adapters.Colors
+import me.gabber235.typewriter.adapters.Entry
+import me.gabber235.typewriter.adapters.Tags
+import me.gabber235.typewriter.adapters.modifiers.OnlyTags
+import me.gabber235.typewriter.entries.data.minecraft.applyGenericEntityData
+import me.gabber235.typewriter.entries.data.minecraft.living.*
+import me.gabber235.typewriter.entry.Ref
+import me.gabber235.typewriter.entry.emptyRef
+import me.gabber235.typewriter.entry.entity.FakeEntity
+import me.gabber235.typewriter.entry.entity.SimpleEntityDefinition
+import me.gabber235.typewriter.entry.entity.SimpleEntityInstance
+import me.gabber235.typewriter.entry.entity.WrapperFakeEntity
+import me.gabber235.typewriter.entry.entries.EntityActivityEntry
+import me.gabber235.typewriter.entry.entries.EntityData
+import me.gabber235.typewriter.entry.entries.EntityProperty
+import me.gabber235.typewriter.extensions.packetevents.metas
+import me.gabber235.typewriter.utils.Sound
+import me.tofaa.entitylib.wrapper.WrapperEntity
+import org.bukkit.Location
+import org.bukkit.entity.Player
+
+@Entry("warden_definition", "A warden entity", Colors.ORANGE, "fluent-emoji-high-contrast:pig-nose")
+@Tags("warden_definition")
+/**
+ * The `WardenDefinition` class is an entry that shows up as a warden in-game.
+ *
+ * ## How could this be used?
+ * This could be used to create a warden entity.
+ */
+class WardenDefinition(
+    override val id: String = "",
+    override val name: String = "",
+    override val displayName: String = "",
+    override val sound: Sound = Sound.EMPTY,
+    @OnlyTags("generic_entity_data", "living_entity_data", "mob_data", "ageable_data")
+    override val data: List<Ref<EntityData<*>>> = emptyList(),
+) : SimpleEntityDefinition {
+    override fun create(player: Player): FakeEntity = WardenEntity(player)
+}
+
+@Entry("warden_instance", "An instance of a warden entity", Colors.YELLOW, "fluent-emoji-high-contrast:pig-nose")
+class WardenInstance(
+    override val id: String = "",
+    override val name: String = "",
+    override val definition: Ref<WardenDefinition> = emptyRef(),
+    override val spawnLocation: Location = Location(null, 0.0, 0.0, 0.0),
+    @OnlyTags("generic_entity_data", "living_entity_data", "mob_data", "ageable_data")
+    override val data: List<Ref<EntityData<*>>> = emptyList(),
+    override val activities: List<Ref<out EntityActivityEntry>> = emptyList(),
+) : SimpleEntityInstance
+
+private class WardenEntity(player: Player) : WrapperFakeEntity(
+    EntityTypes.WARDEN,
+    player,
+) {
+    override fun applyProperty(property: EntityProperty) {
+        when (property) {
+            is AgableProperty -> applyAgeableData(entity, property)
+            else -> {}
+        }
+        if (applyGenericEntityData(entity, property)) return
+        if (applyLivingEntityData(entity, property)) return
+    }
+}
