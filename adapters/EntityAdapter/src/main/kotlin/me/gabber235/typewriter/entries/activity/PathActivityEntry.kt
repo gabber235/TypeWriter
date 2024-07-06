604b2bf08197a22a8ea321c1d206721f44381a88
Gabber235 <gabber200@icloud.com>
2024-07-06 10:06:26 +0200

    Create path activity entry

--- adapters/EntityAdapter/src/main/kotlin/me/gabber235/typewriter/entries/activity/PathActivityEntry.kt
+++ adapters/EntityAdapter/src/main/kotlin/me/gabber235/typewriter/entries/activity/PathActivityEntry.kt
@@ -2,56 +2,63 @@ package me.gabber235.typewriter.entries.activity
 
 import me.gabber235.typewriter.adapters.Colors
 import me.gabber235.typewriter.adapters.Entry
+import me.gabber235.typewriter.adapters.modifiers.Help
 import me.gabber235.typewriter.entry.Ref
 import me.gabber235.typewriter.entry.emptyRef
 import me.gabber235.typewriter.entry.entity.*
 import me.gabber235.typewriter.entry.entries.*
 import me.gabber235.typewriter.entry.roadnetwork.RoadNetworkManager
 import me.gabber235.typewriter.entry.roadnetwork.gps.PointToPointGPS
-import org.koin.core.component.KoinComponent
 import org.koin.java.KoinJavaComponent
 
-
-@Entry("patrol_activity", "Moving around a set of locations", Colors.BLUE, "fa6-solid:route")
+@Entry("path_activity", "Moving along a predefined path", Colors.BLUE, "material-symbols:conversion-path")
 /**
- * The `PatrolActivity` is an activity that makes the entity move around a set of locations.
+ * The `Path Activity` is an activity that moves along a predefined path.
  * The entity will move to each location in the set in order.
- * Once the entity reaches the last location, it will start back at the first location.
+ * Once the entity reaches the last location, it will do the idle activity.
  *
  * ## How could this be used?
- * This could be used to make guards patrol around a set of locations.
+ * This could be used to have a tour guide that moves along the post-important paths.
  */
-class PatrolActivityEntry(
+class PathActivityEntry(
     override val id: String = "",
     override val name: String = "",
     override val roadNetwork: Ref<RoadNetworkEntry> = emptyRef(),
     override val nodes: List<RoadNodeId> = emptyList(),
-) : GenericEntityActivityEntry, RoadNodeCollectionEntry {
-    override fun create(
-        context: ActivityContext,
-        currentLocation: LocationProperty
-    ): EntityActivity<ActivityContext> {
+    @Help("The activity that will be used when the entity is at the final location.")
+    val idleActivity: Ref<out EntityActivityEntry> = emptyRef(),
+): GenericEntityActivityEntry, RoadNodeCollectionEntry {
+    override fun create(context: ActivityContext, currentLocation: LocationProperty): EntityActivity<ActivityContext> {
         if (nodes.isEmpty()) return IdleActivity.create(context, currentLocation)
-        return PatrolActivity(roadNetwork, nodes, currentLocation)
+        return PathActivity(roadNetwork, nodes, currentLocation, idleActivity)
     }
 }
 
-private class PatrolActivity(
+private class PathActivity(
     private val roadNetwork: Ref<RoadNetworkEntry>,
     private val nodes: List<RoadNodeId>,
     private val startLocation: LocationProperty,
-) : EntityActivity<ActivityContext>, KoinComponent {
+    private val idleActivity: Ref<out EntityActivityEntry>,
+) : GenericEntityActivity {
     private var currentLocationIndex = 0
     private var activity: EntityActivity<in ActivityContext>? = null
 
     fun refreshActivity(context: ActivityContext, network: RoadNetwork) {
+        if (nodes.size <= currentLocationIndex) {
+            activity?.dispose(context)
+            activity = idleActivity.get()?.create(context, currentLocation)
+            activity?.initialize(context)
+            return
+        }
+
         val targetNodeId = nodes.getOrNull(currentLocationIndex)
-            ?: throw IllegalStateException("Could not find any node in the nodes list for the patrol activity.")
+            ?: throw IllegalStateException("Could not find any node in the nodes list for the path activity.")
         val targetNode = network.nodes.find { it.id == targetNodeId }
-            ?: throw IllegalStateException("Could not find any node in the nodes list for the patrol activity.")
+            ?: throw IllegalStateException("Could not find any node in the nodes list for the path activity.")
 
         activity?.dispose(context)
-        activity = NavigationActivity(PointToPointGPS(
+        activity = NavigationActivity(
+            PointToPointGPS(
             roadNetwork,
             { currentLocation.toLocation() }) {
             targetNode.location
@@ -74,7 +81,7 @@ private class PatrolActivity(
             ?: throw IllegalStateException("Could not find any node in the nodes list for the patrol activity.")
 
         val index = nodes.indexOf(closestNode.id)
-        currentLocationIndex = (index + 1) % nodes.size
+        currentLocationIndex = (index + 1)
         refreshActivity(context, network)
     }
 
@@ -91,7 +98,7 @@ private class PatrolActivity(
 
         val result = activity?.tick(context)
         if (result == TickResult.IGNORED) {
-            currentLocationIndex = (currentLocationIndex + 1) % nodes.size
+            currentLocationIndex = (currentLocationIndex + 1)
             refreshActivity(context, network)
         }
 
