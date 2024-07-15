66a7957151bba7cbb268bfb2fa8c642098ec08df
Gabber235 <gabber200@icloud.com>
2024-07-15 08:01:02 +0200

    Change base url

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
     url: 'https://gabber235.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/TypeWriter/',
+    baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
