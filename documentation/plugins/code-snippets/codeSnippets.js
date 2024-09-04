88309fbbde8339f0947c53f4be6e30cc1a8a0eef
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-09-04 20:20:46 +0200

    [Docs] Revert some changes
    This reverts commit 79979b47e4e645e1107221228e89e74278bcf1d9.

--- documentation/plugins/code-snippets/codeSnippets.js
+++ documentation/plugins/code-snippets/codeSnippets.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const codeBlockRegex = /\/\/<(?<closing>\/)?code-block:(?<tag>[a-zA-Z0-9_]*)>$/;
+const codeBockRegex = /\/\/<(?<closing>\/)?code-block:(?<tag>[a-zA-Z0-9_]*)>$/;
 
 module.exports = function() {
     const snippets = {};
@@ -10,48 +10,26 @@ module.exports = function() {
         const content = fs.readFileSync(filePath, 'utf8');
         const lines = content.split('\n');
         const blocks = {};
-        const blockStartLines = {};
 
         lines.forEach((line, index) => {
-            const match = line.match(codeBlockRegex);
+            const match = line.match(codeBockRegex);
             if (match) {
                 const { closing, tag } = match.groups;
                 if (closing) {
                     const code = blocks[tag];
                     if (!code) {
-                        // Handle the case where a closing tag does not have an opening tag
-                        snippets[tag] = {
-                            file: relativeFilePath,
-                            content: "Not found", // Temporary fix so the docs can be build...
-                            startLine: "N/A",
-                            endLine: index + 1
-                        };
-                    } else {
-                        // Close the block and save the snippet
-                        blocks[tag] = null;
-                        snippets[tag] = {
-                            file: relativeFilePath,
-                            content: code.join('\n'),
-                            startLine: blockStartLines[tag] + 1,
-                            endLine: index + 1
-                        };
+                        throw new Error(`Code block not closed: ${tag} (${relativeFilePath}:${index + 1})`);
                     }
-                } else {
-                    if (blocks[tag]) {
-                        // Handle the case where an opening tag already exists
-                        snippets[tag] = {
-                            file: relativeFilePath,
-                            content: "Not found", // Temporary fix so the docs can be build...
-                            startLine: blockStartLines[tag] + 1,
-                            endLine: index + 1
-                        };
-                    }
-                    // Start a new block
-                    blocks[tag] = [];
-                    blockStartLines[tag] = index;
+                    blocks[tag] = null;
+                    snippets[tag] = {
+                        file: relativeFilePath,
+                        content: code.join('\n')
+                    };
+                    return;
                 }
+
+                blocks[tag] = [];
             } else {
-                // Add lines to the current blocks
                 for (const tag in blocks) {
                     const code = blocks[tag];
                     if (!code) continue;
@@ -63,14 +41,11 @@ module.exports = function() {
         // Handle any unclosed blocks
         for (const tag in blocks) {
             const code = blocks[tag];
-            if (code) {
-                snippets[tag] = {
-                    file: relativeFilePath,
-                    content: "Not found", // Temporary fix so the docs can be build...
-                    startLine: blockStartLines[tag] + 1,
-                    endLine: "N/A"
-                };
-            }
+            if (!code) continue;
+            snippets[tag] = {
+                file: relativeFilePath,
+                content: code.join('\n')
+            };
         }
     }
 
@@ -83,13 +58,7 @@ module.exports = function() {
             if (stat.isDirectory()) {
                 traverseDirectory(filePath, baseDir);
             } else if (path.extname(filePath) === '.kt') {
-                try {
-                    processFile(filePath, relativeFilePath);
-                } catch (error) {
-                    // Improve error handling by showing which file failed
-                    console.error(`Error processing file: ${relativeFilePath}`);
-                    throw error;
-                }
+                processFile(filePath, relativeFilePath);
             }
         });
     }
@@ -98,11 +67,13 @@ module.exports = function() {
     const adaptersDir = path.resolve(__dirname, '../../../extensions/_DocsExtension');
     traverseDirectory(adaptersDir, adaptersDir);
 
+    // console.log(`Exporting ${Object.keys(snippets).length} snippets: ${Object.keys(snippets)}`);
+
     this.cacheable(false);
 
     const code = `${JSON.stringify(snippets, null, 2)}`;
 
-    fs.writeFileSync(path.resolve(__dirname, 'snippets.json'), code);
+    fs.writeSync(fs.openSync(path.resolve(__dirname, 'snippets.json'), 'w'), code);
 
     return `export default ${JSON.stringify(snippets, null, 2)}`;
 };
