c1c04717180d424ca1ed1d07dffbc1e538b720e9
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-09-05 18:13:50 +0200

    [Docs] Navbar changes

---
+++ documentation/src/theme/NavbarItem/DocSidebarNavbarItem.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  useActiveDocContext,
+  useLayoutDocsSidebar,
+  useActiveVersion,
+  useLatestVersion,
+} from '@docusaurus/plugin-content-docs/client';
+import DefaultNavbarItem from '@theme/NavbarItem/DefaultNavbarItem';
+import type {Props} from '@theme/NavbarItem/DocSidebarNavbarItem';
+
+export default function DocSidebarNavbarItem({
+  sidebarId,
+  label,
+  docsPluginId,
+  ...props
+}: Props): JSX.Element {
+  const {activeDoc} = useActiveDocContext(docsPluginId);
+  const sidebarLink = useLayoutDocsSidebar(sidebarId, docsPluginId).link;
+
+  const activeVersion = useActiveVersion(docsPluginId)?.name;
+
+  if (!sidebarLink) {
+    throw new Error(
+      `DocSidebarNavbarItem: Sidebar with ID "${sidebarId}" doesn't have anything to be linked to.`,
+    );
+  }
+
+  // Adjust label based on the active version
+  console.log(activeVersion);
+  console.log(label);
+  console.log(activeVersion <= '0.5.0');
+  console.log(sidebarLink.path);
+  const adjustedLabel =
+    label === 'Extensions' && activeVersion && activeVersion <= '0.5.0'
+      ? 'Adapters'
+      : label;
+
+  return (
+    <DefaultNavbarItem
+      exact
+      {...props}
+      isActive={() => activeDoc?.sidebar === sidebarId}
+      label={adjustedLabel ?? sidebarLink.label}
+      to={sidebarLink.path}
+    />
+  );
+}
