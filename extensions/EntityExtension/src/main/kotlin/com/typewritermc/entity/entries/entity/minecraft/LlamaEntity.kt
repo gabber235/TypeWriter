03703b0b15f92d9e22ffcdb14af419f742cb21bf
Kerzinator_24 <kerzinator24@gmail.com>
2024-09-10 21:09:35 +0200

    added llama and trader llama (updated to 0.6) as originally requested in https://discord.com/channels/1054708062520360960/1277280772448845977

---
+++ extensions/EntityExtension/src/main/kotlin/com/typewritermc/entity/entries/entity/minecraft/LlamaEntity.kt
@@ -0,0 +1,78 @@
+package com.typewritermc.entity.entries.entity.minecraft
+
+import com.github.retrooper.packetevents.protocol.entity.type.EntityTypes
+import com.typewritermc.core.books.pages.Colors
+import com.typewritermc.core.entries.Ref
+import com.typewritermc.core.entries.emptyRef
+import com.typewritermc.core.extension.annotations.Entry
+import com.typewritermc.core.extension.annotations.OnlyTags
+import com.typewritermc.core.extension.annotations.Tags
+import com.typewritermc.core.utils.point.Position
+import com.typewritermc.engine.paper.entry.entity.FakeEntity
+import com.typewritermc.engine.paper.entry.entity.SimpleEntityDefinition
+import com.typewritermc.engine.paper.entry.entity.SimpleEntityInstance
+import com.typewritermc.engine.paper.entry.entries.EntityData
+import com.typewritermc.engine.paper.entry.entries.EntityProperty
+import com.typewritermc.engine.paper.entry.entries.SharedEntityActivityEntry
+import com.typewritermc.engine.paper.utils.Sound
+import com.typewritermc.entity.entries.data.minecraft.applyGenericEntityData
+import com.typewritermc.entity.entries.data.minecraft.living.AgableProperty
+import com.typewritermc.entity.entries.data.minecraft.living.applyAgeableData
+import com.typewritermc.entity.entries.data.minecraft.living.applyLivingEntityData
+import com.typewritermc.entity.entries.data.minecraft.living.horse.*
+import com.typewritermc.entity.entries.entity.WrapperFakeEntity
+import org.bukkit.entity.Player
+
+@Entry("llaama_definition", "A llama entity", Colors.ORANGE, "ph:llama-fill")
+@Tags("llama_definition")
+/**
+ * The `LlamaDefinition` class is an entry that represents a llama entity.
+ *
+ * ## How could this be used?
+ * This could be used to create a llama entity.
+ */
+class LlamaDefinition(
+    override val id: String = "",
+    override val name: String = "",
+    override val displayName: String = "",
+    override val sound: Sound = Sound.EMPTY,
+    @OnlyTags("generic_entity_data", "living_entity_data", "mob_data", "ageable_data", "llama_data", "chested_horse_data")
+    override val data: List<Ref<EntityData<*>>> = emptyList(),
+) : SimpleEntityDefinition {
+    override fun create(player: Player): FakeEntity = LlamaEntity(player)
+}
+
+@Entry("llama_instance", "An instance of a llama entity", Colors.YELLOW, "ph:llama-fill")
+/**
+ * The `LlamaInstance` class is an entry that represents an instance of a llama entity.
+ *
+ * ## How could this be used?
+ *
+ * This could be used to create a llama entity.
+ */
+class LlamaInstance(
+    override val id: String = "",
+    override val name: String = "",
+    override val definition: Ref<LlamaDefinition> = emptyRef(),
+    override val spawnLocation: Position = Position.ORIGIN,
+    @OnlyTags("generic_entity_data", "living_entity_data", "mob_data", "ageable_data", "llama_data", "chested_horse_data")
+    override val data: List<Ref<EntityData<*>>> = emptyList(),
+    override val activity: Ref<out SharedEntityActivityEntry> = emptyRef(),
+) : SimpleEntityInstance
+
+private class LlamaEntity(player: Player) : WrapperFakeEntity(
+    EntityTypes.LLAMA,
+    player,
+) {
+    override fun applyProperty(property: EntityProperty) {
+        when (property) {
+            is AgableProperty -> applyAgeableData(entity, property)
+            is LlamaVariantProperty -> applyLlamaVariantData(entity, property)
+            is LlamaCarpetColorProperty -> applyLlamaCarpetColorData(entity, property)
+            is ChestedHorseChestProperty -> applyChestedHorseChestData(entity, property)
+            else -> {}
+        }
+        if (applyGenericEntityData(entity, property)) return
+        if (applyLivingEntityData(entity, property)) return
+    }
+}
