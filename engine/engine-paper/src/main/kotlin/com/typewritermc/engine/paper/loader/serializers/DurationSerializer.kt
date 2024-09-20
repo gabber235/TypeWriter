c63b427efec3fcf7de1ae6abb5b8d6d697eecc3c
Gabber235 <gabber200@icloud.com>
2024-09-20 11:28:49 +0200

    Fix GSON null serialisation

--- engine/engine-paper/src/main/kotlin/com/typewritermc/engine/paper/loader/serializers/DurationSerializer.kt
+++ engine/engine-paper/src/main/kotlin/com/typewritermc/engine/paper/loader/serializers/DurationSerializer.kt
@@ -4,7 +4,7 @@ import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonElement
 import com.google.gson.JsonPrimitive
 import com.google.gson.JsonSerializationContext
-import com.typewritermc.loader.DataSerializer
+import com.typewritermc.core.serialization.DataSerializer
 import java.lang.reflect.Type
 import java.time.Duration
 
