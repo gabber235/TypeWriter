ddf4a55a82b161ea51cd05343ad997d09e5696b8
Gabber235 <gabber200@icloud.com>
2024-09-26 11:49:59 +0200

    Fix Default annotation in engine

--- engine/engine-core/src/main/kotlin/com/typewritermc/core/extension/annotations/Default.kt
+++ engine/engine-core/src/main/kotlin/com/typewritermc/core/extension/annotations/Default.kt
@@ -3,7 +3,7 @@ package com.typewritermc.core.extension.annotations
 import org.intellij.lang.annotations.Language
 
 @Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.SOURCE)
+@Retention(AnnotationRetention.RUNTIME)
 /**
  * This allows you to specify a default value for the field.
  * The default value must be a valid JSON string.
