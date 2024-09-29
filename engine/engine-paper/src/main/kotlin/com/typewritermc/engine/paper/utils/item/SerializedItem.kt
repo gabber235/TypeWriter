c684a8528289aac7f4d10af0762f6dc220025bff
Gabber235 <gabber200@icloud.com>
2024-09-29 13:20:17 +0200

    Serialized Item allow to specify amount

--- engine/engine-paper/src/main/kotlin/com/typewritermc/engine/paper/utils/item/SerializedItem.kt
+++ engine/engine-paper/src/main/kotlin/com/typewritermc/engine/paper/utils/item/SerializedItem.kt
@@ -2,6 +2,7 @@ package com.typewritermc.engine.paper.utils.item
 
 import com.typewritermc.core.books.pages.Colors
 import com.typewritermc.core.extension.annotations.AlgebraicTypeInfo
+import com.typewritermc.core.extension.annotations.Default
 import com.typewritermc.engine.paper.utils.plainText
 import org.bukkit.Material
 import org.bukkit.entity.Player
@@ -14,18 +15,25 @@ import kotlin.io.encoding.ExperimentalEncodingApi
 class SerializedItem(
     private val material: Material = Material.AIR,
     private val name: String = material.name,
+
+    @Default("1")
+    private val amount: Int = 1,
+
     private val bytes: String = "", // Base64 encoded bytes
 ) : Item {
     constructor(itemStack: ItemStack) : this(
         itemStack.type,
         itemStack.itemMeta.displayName()?.plainText() ?: itemStack.type.name,
+        itemStack.amount,
         Base64.encode(itemStack.serializeAsBytes())
     )
 
     @delegate:Transient
     private val itemStack: ItemStack by lazy(LazyThreadSafetyMode.NONE) {
         val bytes = Base64.decode(bytes)
-        ItemStack.deserializeBytes(bytes)
+        ItemStack.deserializeBytes(bytes).apply {
+            amount = this@SerializedItem.amount
+        }
     }
 
     override fun build(player: Player?): ItemStack = itemStack
