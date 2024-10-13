c79f6242c0f2c027fa4cfede0d377f0fcc81b00f
Kerzinator_24 <kerzinator24@gmail.com>
2024-10-13 13:50:03 +0200

    added PillagerEntity and VindicatorEntity

---
+++ extensions/EntityExtension/src/main/kotlin/com/typewritermc/entity/entries/entity/minecraft/VindicatorEntity.kt
@@ -0,0 +1,61 @@
+package com.typewritermc.entity.entries.entity.minecraft
+
+import com.github.retrooper.packetevents.protocol.entity.type.EntityTypes
+import com.typewritermc.core.books.pages.Colors
+import com.typewritermc.core.entries.Ref
+import com.typewritermc.core.entries.emptyRef
+import com.typewritermc.core.extension.annotations.Entry
+import com.typewritermc.core.extension.annotations.OnlyTags
+import com.typewritermc.core.extension.annotations.Tags
+import com.typewritermc.core.utils.point.Position
+import com.typewritermc.engine.paper.entry.entity.FakeEntity
+import com.typewritermc.engine.paper.entry.entity.SimpleEntityDefinition
+import com.typewritermc.engine.paper.entry.entity.SimpleEntityInstance
+import com.typewritermc.engine.paper.entry.entries.EntityData
+import com.typewritermc.engine.paper.entry.entries.EntityProperty
+import com.typewritermc.engine.paper.entry.entries.SharedEntityActivityEntry
+import com.typewritermc.engine.paper.utils.Sound
+import com.typewritermc.entity.entries.data.minecraft.applyGenericEntityData
+import com.typewritermc.entity.entries.data.minecraft.living.applyLivingEntityData
+import com.typewritermc.entity.entries.entity.WrapperFakeEntity
+import org.bukkit.entity.Player
+
+@Entry("vindicator_definition", "A vindicator entity", Colors.ORANGE, "game-icons:axe-invader")
+@Tags("vindicator_definition")
+/**
+ * The `VindicatorDefinition` class is an entry that shows up as a vindicator in-game.
+ *
+ * ## How could this be used?
+ * This could be used to create a vindicator entity.
+ */
+class VindicatorDefinition(
+    override val id: String = "",
+    override val name: String = "",
+    override val displayName: String = "",
+    override val sound: Sound = Sound.EMPTY,
+    @OnlyTags("generic_entity_data", "living_entity_data", "mob_data")
+    override val data: List<Ref<EntityData<*>>> = emptyList(),
+) : SimpleEntityDefinition {
+    override fun create(player: Player): FakeEntity = VindicatorEntity(player)
+}
+
+@Entry("vindicator_instance", "An instance of a vindicator entity", Colors.YELLOW, "game-icons:axe-invader")
+class VindicatorInstance(
+    override val id: String = "",
+    override val name: String = "",
+    override val definition: Ref<VindicatorDefinition> = emptyRef(),
+    override val spawnLocation: Position = Position.ORIGIN,
+    @OnlyTags("generic_entity_data", "living_entity_data", "mob_data")
+    override val data: List<Ref<EntityData<*>>> = emptyList(),
+    override val activity: Ref<out SharedEntityActivityEntry> = emptyRef(),
+) : SimpleEntityInstance
+
+private class VindicatorEntity(player: Player) : WrapperFakeEntity(
+    EntityTypes.VINDICATOR,
+    player,
+) {
+    override fun applyProperty(property: EntityProperty) {
+        if (applyGenericEntityData(entity, property)) return
+        if (applyLivingEntityData(entity, property)) return
+    }
+}
