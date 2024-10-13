6e896f80050f8d91d353403bca17d66a2f79be9f
Kerzinator_24 <kerzinator24@gmail.com>
2024-10-13 13:50:03 +0200

    added PillagerEntity and VindicatorEntity

---
+++ extensions/EntityExtension/src/main/kotlin/com/typewritermc/entity/entries/entity/minecraft/PillagerEntity.kt
@@ -0,0 +1,61 @@
+package com.typewritermc.entity.entries.entity.minecraft
+
+import com.github.retrooper.packetevents.protocol.entity.type.EntityTypes
+import com.typewritermc.core.books.pages.Colors
+import com.typewritermc.core.entries.Ref
+import com.typewritermc.core.entries.emptyRef
+import com.typewritermc.core.extension.annotations.Entry
+import com.typewritermc.core.extension.annotations.OnlyTags
+import com.typewritermc.core.extension.annotations.Tags
+import com.typewritermc.core.utils.point.Position
+import com.typewritermc.engine.paper.entry.entity.FakeEntity
+import com.typewritermc.engine.paper.entry.entity.SimpleEntityDefinition
+import com.typewritermc.engine.paper.entry.entity.SimpleEntityInstance
+import com.typewritermc.engine.paper.entry.entries.EntityData
+import com.typewritermc.engine.paper.entry.entries.EntityProperty
+import com.typewritermc.engine.paper.entry.entries.SharedEntityActivityEntry
+import com.typewritermc.engine.paper.utils.Sound
+import com.typewritermc.entity.entries.data.minecraft.applyGenericEntityData
+import com.typewritermc.entity.entries.data.minecraft.living.applyLivingEntityData
+import com.typewritermc.entity.entries.entity.WrapperFakeEntity
+import org.bukkit.entity.Player
+
+@Entry("pillager_definition", "A pillager entity", Colors.ORANGE, "game-icons:crossbow")
+@Tags("pillager_definition")
+/**
+ * The `PillagerDefinition` class is an entry that shows up as a pillager in-game.
+ *
+ * ## How could this be used?
+ * This could be used to create a pillager entity.
+ */
+class PillagerDefinition(
+    override val id: String = "",
+    override val name: String = "",
+    override val displayName: String = "",
+    override val sound: Sound = Sound.EMPTY,
+    @OnlyTags("generic_entity_data", "living_entity_data", "mob_data")
+    override val data: List<Ref<EntityData<*>>> = emptyList(),
+) : SimpleEntityDefinition {
+    override fun create(player: Player): FakeEntity = PillagerEntity(player)
+}
+
+@Entry("pillager_instance", "An instance of a pillager entity", Colors.YELLOW, "game-icons:crossbow")
+class PillagerInstance(
+    override val id: String = "",
+    override val name: String = "",
+    override val definition: Ref<PillagerDefinition> = emptyRef(),
+    override val spawnLocation: Position = Position.ORIGIN,
+    @OnlyTags("generic_entity_data", "living_entity_data", "mob_data")
+    override val data: List<Ref<EntityData<*>>> = emptyList(),
+    override val activity: Ref<out SharedEntityActivityEntry> = emptyRef(),
+) : SimpleEntityInstance
+
+private class PillagerEntity(player: Player) : WrapperFakeEntity(
+    EntityTypes.PILLAGER,
+    player,
+) {
+    override fun applyProperty(property: EntityProperty) {
+        if (applyGenericEntityData(entity, property)) return
+        if (applyLivingEntityData(entity, property)) return
+    }
+}
