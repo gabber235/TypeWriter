7ee54f959134be41f976c0a89f939b599fb73e35
Gabber235 <gabber200@icloud.com>
2024-11-10 19:07:15 +0100

    Write variable documentation

--- documentation/src/components/Figure/index.tsx
+++ documentation/src/components/Figure/index.tsx
@@ -6,9 +6,9 @@ interface FigureProps extends Props {
 
 export default function Figure({ title, ...props }: FigureProps) {
     return (
-        <figure className="flex flex-col items-center justify-center space-y-2">
+        <div className="flex flex-col items-center justify-center space-y-2">
             <Image {...props} />
             <figcaption className="text-center text-gray-500 dark:text-gray-400">{title}</figcaption>
-        </figure>
+        </div>
     );
 }
