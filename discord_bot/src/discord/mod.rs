255af5790db04532c4f84670c4710bf6dc34e821
Gabber235 <gabber200@icloud.com>
2024-11-19 17:03:58 +0100

    Remove Winston Intake

--- discord_bot/src/discord/mod.rs
+++ discord_bot/src/discord/mod.rs
@@ -1,23 +1,19 @@
 mod close_ticket;
 mod create_task;
 mod post_in_questions;
-mod support_answering;
 mod task_fixed;
 mod thread_archiving;
 mod thread_cleanup;
 mod thread_closed_blocker;
-mod thread_intake;
 mod thread_support_answering;
 mod ticket_reopen;
 
 pub use close_ticket::*;
 pub use create_task::*;
 pub use post_in_questions::*;
-pub use support_answering::*;
 pub use task_fixed::*;
 pub use thread_archiving::*;
 pub use thread_cleanup::*;
 pub use thread_closed_blocker::*;
-pub use thread_intake::*;
 pub use thread_support_answering::*;
 pub use ticket_reopen::*;
