c5b8b6086ef39ae49c2cbb3d84d089d05755b9da
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-11-20 17:44:43 +0100

    [Bot] Test bot commit

--- discord_bot/src/discord/mod.rs
+++ discord_bot/src/discord/mod.rs
@@ -7,7 +7,7 @@ mod thread_cleanup;
 mod thread_closed_blocker;
 mod thread_support_answering;
 mod ticket_reopen;
-
+mod follow_up;
 pub use close_ticket::*;
 pub use create_task::*;
 pub use post_in_questions::*;
@@ -17,3 +17,4 @@ pub use thread_cleanup::*;
 pub use thread_closed_blocker::*;
 pub use thread_support_answering::*;
 pub use ticket_reopen::*;
+pub use follow_up::*;
