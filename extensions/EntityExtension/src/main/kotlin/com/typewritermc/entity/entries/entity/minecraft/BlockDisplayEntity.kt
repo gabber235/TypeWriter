3c543291efa71ca106880d7ab70393508c3cd0af
Gabber235 <gabber200@icloud.com>
2024-11-24 18:02:10 +0100

    Create block display entity

--- extensions/EntityExtension/src/main/kotlin/com/typewritermc/entity/entries/entity/minecraft/BlockDisplayEntity.kt
+++ extensions/EntityExtension/src/main/kotlin/com/typewritermc/entity/entries/entity/minecraft/BlockDisplayEntity.kt
@@ -14,64 +14,48 @@ import com.typewritermc.engine.paper.entry.entity.SimpleEntityInstance
 import com.typewritermc.engine.paper.entry.entries.*
 import com.typewritermc.engine.paper.extensions.packetevents.meta
 import com.typewritermc.engine.paper.utils.Sound
-import com.typewritermc.engine.paper.utils.asMini
 import com.typewritermc.entity.entries.data.minecraft.applyGenericEntityData
 import com.typewritermc.entity.entries.data.minecraft.display.applyDisplayEntityData
-import com.typewritermc.entity.entries.data.minecraft.display.text.*
+import com.typewritermc.entity.entries.data.minecraft.display.block.BlockProperty
+import com.typewritermc.entity.entries.data.minecraft.display.block.applyBlockData
 import com.typewritermc.entity.entries.entity.WrapperFakeEntity
-import me.tofaa.entitylib.meta.display.TextDisplayMeta
+import me.tofaa.entitylib.meta.display.BlockDisplayMeta
 import org.bukkit.entity.Player
 
-@Entry("text_display_definition", "A text display entity", Colors.ORANGE, "material-symbols:text-ad-rounded")
-@Tags("text_display_definition")
+@Entry("block_display_definition", "A block display entity", Colors.ORANGE, "heroicons:cube-transparent-16-solid")
+@Tags("block_display_definition")
 /**
- * The `TextDisplayDefinition` class is an entry that represents a text display entity.
+ * The `BlockDisplayDefinition` class is an entry that represents a block display entity.
  *
  * ## How could this be used?
- * This could be used to create an entity that displays text.
+ * This could be used to create an entity that displays a block.
  */
-class TextDisplayDefinition(
+class BlockDisplayDefinition(
     override val id: String = "",
     override val name: String = "",
     override val displayName: Var<String> = ConstVar(""),
     override val sound: Sound = Sound.EMPTY,
-    @OnlyTags("generic_entity_data", "display_data", "lines", "text_display_data")
+    @OnlyTags("generic_entity_data", "display_data", "block_display_data")
     override val data: List<Ref<EntityData<*>>> = emptyList(),
 ) : SimpleEntityDefinition {
-    override fun create(player: Player): FakeEntity = TextDisplayEntity(player)
+    override fun create(player: Player): FakeEntity = BlockDisplayEntity(player)
 }
 
-@Entry(
-    "text_display_instance",
-    "An instance of a text display entity",
-    Colors.YELLOW,
-    "material-symbols:text-ad-rounded"
-)
-class TextDisplayInstance(
+@Entry("block_display_instance", "An instance of a block display entity", Colors.YELLOW, "heroicons:cube-transparent-16-solid")
+class BlockDisplayInstance(
     override val id: String = "",
     override val name: String = "",
-    override val definition: Ref<TextDisplayDefinition> = emptyRef(),
+    override val definition: Ref<BlockDisplayDefinition> = emptyRef(),
     override val spawnLocation: Position = Position.ORIGIN,
-    @OnlyTags("generic_entity_data", "display_data", "lines", "text_display_data")
+    @OnlyTags("generic_entity_data", "display_data", "block_display_data")
     override val data: List<Ref<EntityData<*>>> = emptyList(),
     override val activity: Ref<out SharedEntityActivityEntry> = emptyRef(),
 ) : SimpleEntityInstance
 
-open class TextDisplayEntity(player: Player) : WrapperFakeEntity(
-    EntityTypes.TEXT_DISPLAY,
-    player,
-) {
+open class BlockDisplayEntity(player: Player) : WrapperFakeEntity(EntityTypes.BLOCK_DISPLAY, player) {
     override fun applyProperty(property: EntityProperty) {
         when (property) {
-            is LinesProperty -> entity.meta<TextDisplayMeta>(preventNotification = true) {
-                text = property.lines.asMini()
-            }
-
-            is BackgroundColorProperty -> applyBackgroundColorData(entity, property)
-            is TextOpacityProperty -> applyTextOpacityData(entity, property)
-            is LineWidthProperty -> applyLineWidthData(entity, property)
-            is ShadowProperty -> applyShadowData(entity, property)
-            is SeeThroughProperty -> applySeeThroughData(entity, property)
+            is BlockProperty -> applyBlockData(entity, property)
             else -> {}
         }
         if (applyGenericEntityData(entity, property)) return
