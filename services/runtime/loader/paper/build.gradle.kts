ae1b4cc6563cbf14c6b87b2e709f56af33c02408
Gabber235 <gabber200@icloud.com>
2026-09-03 09:04:04 +0900

    build(services): emit native Paper plugin metadata

--- services/runtime/loader/paper/build.gradle.kts
+++ services/runtime/loader/paper/build.gradle.kts
@@ -1,16 +1,28 @@
-import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
-
 plugins {
     id("com.typewritermc.basic-conventions")
-    id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.3.1"
+    id("xyz.jpenilla.resource-factory-paper-convention") version "1.3.1"
 }
 
-bukkitPluginYaml {
-    name = "TypewriterLoader"
+paperPluginYaml {
+    name = "Typewriter"
+    description = "Next Generation Story Telling Plugin"
+    authors = listOf("gabber235", "Marten-Mrfc", "steveb05")
+    website = "https://docs.typewritermc.com"
     version = project.version.toString()
+
     main = "com.typewritermc.loader.paper.TypewriterLoaderPlugin"
-    apiVersion = "1.21"
-    load = BukkitPluginYaml.PluginLoadOrder.STARTUP
+    apiVersion = "1.21.3"
+
+    foliaSupported = false
+
+//    Possibly in the future this will be used. For now its not used yet.
+//    dependencies {
+//        server("packetevents", load = PaperPluginYaml.Load.BEFORE, required = true, joinClasspath = true)
+//        server("PlaceholderAPI", load = PaperPluginYaml.Load.BEFORE, required = false, joinClasspath = true)
+//        server("floodgate", load = PaperPluginYaml.Load.BEFORE, required = false, joinClasspath = true)
+//    }
+//
+//    loader = "com.typewritermc.engine.paper.TypewriterPaperLoader"
 }
 
 repositories {
