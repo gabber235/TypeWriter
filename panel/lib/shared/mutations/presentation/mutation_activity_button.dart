121b328f4c1580a2f04ad366c4ea71ddddee332c
Gabber235 <gabber200@icloud.com>
2026-09-12 06:15:23 +0900

    feat(panel): expose actionable draft save diagnostics

--- panel/lib/shared/mutations/presentation/mutation_activity_button.dart
+++ panel/lib/shared/mutations/presentation/mutation_activity_button.dart
@@ -1,6 +1,7 @@
 import "dart:math" as math;
 
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:typewriter_panel/typewriter_panel.dart";
 
